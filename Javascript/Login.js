0e4ad767ef6d4abac8ba01c2d338c951a778365d
Miyazawa93 <Kristinhoel@hotmail.com>
2017-04-19 12:27:50 +0200

    Create Login.js

---
+++ Javascript/Login.js
@@ -0,0 +1,40 @@
+$("#submit").click(function() {
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var hashed = CryptoJS.MD5(password).toString();
+
+  var selectedCompany = document.getElementById("companyID");
+  var uploadCompany = selectedCompany.options[selectedCompany.selectedIndex].value;
+
+  console.log(email);
+  console.log(password);
+  console.log(hashed);
+  console.log(uploadCompany);
+
+  $.ajax({
+    url: "https://localhost:3000/api/login/staff",
+    type: "POST",
+    cache: false,
+    crossOrigin: true,
+    headers: {'Content-Type': 'application/json'},
+    useDefaultXhrHeader: false,
+    dataType: 'json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest"); 
+      xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+      xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+      xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Range, Content-Disposition, Content-Description');
+    },
+    data: {
+      "companyid": uploadCompany,
+      "email": email,
+      "passwordhash": hashed
+    },
+    success: function(response) {
+      console.log('Successfully retrieved ' + response);
+    },
+    error: function(response) {
+      console.log('Failed ' + response.status + ' ' + response.statusText);
+    }
+  }).done();
+});
